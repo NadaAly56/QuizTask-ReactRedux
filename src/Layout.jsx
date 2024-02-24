c0888c8f776989a331985c6b9ddb7810e3668fb3
nada <nada.aly5613@gmail.com>
2024-02-24 04:02:59 +0200

    Final commit

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,46 +1,41 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import AnswersList from './components/AnswersList.jsx'
 import Button from './components/Button.jsx'
 import Header from './components/Header'
 import Question from './components/Question.jsx'
-import { useDispatch, useSelector } from 'react-redux'
-import { fetchData } from './redux/questionsSlice.js'
 import Loader from './components/Loader.jsx'
 import Start from './components/Start.jsx'
+import { useSelector } from 'react-redux'
 export default function Layout() {
-    const { status, error } = useSelector((store)=> store.questions)
-    const dispatch = useDispatch()
-    // useEffect(()=>{
-    //   dispatch(fetchData())
-    // },[dispatch])
-  
-  return (
-    <main className="overflow-hidden">
-        {
-            status === "" && <Start />
-        }
-    {
-      status === "loading" && <Loader />
-    }
-    { status === "failed" && <div>{error}</div>}
-    {
-      status === "ready" && 
-      <>
-      <Header />
-    <form>
-      <div className="quiz-main">
-        <div className="container show-section">
-        <section className='steps'>
-          <Question />
-          <AnswersList />
-          <Button />
-        </section>
-        </div>
-      </div>
-    </form>
-      </>
-      
-    }
-  </main>
-  )
+    const { status, error } = useSelector((store) => store.questions)
+
+    return (
+        <main className="overflow-hidden">
+            {
+                status === "" && <Start />
+            }
+            {
+                status === "loading" && <Loader />
+            }
+            {status === "failed" && <div>{error}</div>}
+            {
+                status === "ready" &&
+                <>
+                    <Header />
+                    <form>
+                        <div className="quiz-main">
+                            <div className="container show-section">
+                                <section className='steps'>
+                                    <Question />
+                                    <AnswersList />
+                                    <Button />
+                                </section>
+                            </div>
+                        </div>
+                    </form>
+                </>
+
+            }
+        </main>
+    )
 }
